2e8e3f2bc64b6645b3c549cd1c0c3e3e8d0bbef7
Breno-RM <78373085+Breno-RM@users.noreply.github.com>
2026-05-09 21:48:20 -0300

    Update controle.py

--- Controle/controle.py
+++ Controle/controle.py
@@ -1,13 +1,14 @@
 import control as ctl
 import numpy as np
 
-from core.core import Core
+from Core.core import Core
+from Data.data import Data
 
 class Controle:
     
-    def __init__(self):
+    def __init__(self, Data = Data()):
         # Instância da classe Core
-        self.core = Core()
+        self.core = Core(Data)
 
         # Variáveis que serão preenchidas posteriormente
         self.time = None
@@ -62,10 +63,76 @@ class Controle:
         # Retorna a resposta controlada
         return response
 
+    def set_pidzoh(self, Kp, Ki, Kd, N, Ts, umin=0.0, umax=255.0):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.N = N
+        self.Ts = Ts
+        
+        # Saturação da saída
+        # O umin e umax são os valor limite de saturação do sinal de controle (u)
+        # Pode usar os valores min/max da fonte ou min/max de funcionamento da planta
+        self.umin = umin
+        self.umax = umax
+
+        # Coeficientes do derivativo filtrado
+        # ad = e^(-N * Ts)
+        self.ad = np.exp(-N * Ts)
+        # bd = Kd * N * (1 - ad)
+        self.bd = Kd * N * (1.0 - self.ad)
+
+        # Estados internos
+        self.integral = 0.0
+        self.derivative = 0.0
+        self.e_prev = 0.0
+        self.pid_zoh_erro = None
+
+    def pidzoh(self, setpoint, feedback):
+        # O setpoint NÃO está em porcentagem
+
+        # Erro
+        pid_erro = setpoint - feedback
+
+        # Proporcional
+        # P[k] = Kp * pid_erro[k]
+        proportional = self.Kp * pid_erro
+
+        # Integral (candidato)
+        # Ic[k] = I[k] + Ki * Ts * pid_erro[k]
+        integral_candidate = self.integral + self.Ki * self.Ts * pid_erro
+
+        # Erro derivativo
+        # derro[k] = pid_erro[k] - pid_erro[k-1]
+        derro = pid_erro - self.e_prev
+
+        # Derivativo (candidato)
+        # Dc[k] = ad * D[k-1] + bd * derro[k] 
+        derivative_candidate = self.ad * self.derivative + self.bd * derro
+
+        # Saída não saturada
+        # u_unsat[k] = P[k] + Ic[k] + Dc[k]
+        u_unsat = proportional + integral_candidate + derivative_candidate
+
+        # Saturação
+        u = max(self.umin, min(self.umax, u_unsat))
+
+        # Anti-windup simples:
+        # só aceita a integral se não houver saturação
+        if u == u_unsat:
+            self.integral = integral_candidate
+
+        # Atualiza estados
+        self.derivative = derivative_candidate
+        self.e_prev = pid_erro
+        self.pid_zoh_erro = pid_erro
+        # O sinal de controle (u) varia de acordo com os limites de saturação
+        return u
+
     def get_pid_erro(self):
         # Retorna o erro do PID
         return self.pid_erro
-
-
-
-
+    
+    def get_pid_zoh_erro(self):
+        # Retorna o erro do PID
+        return self.pid_zoh_erro
